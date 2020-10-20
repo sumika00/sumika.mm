2da86605dfa9e2a4239a400ded2fa9e931dc7d66
sumika00 <zm18454@josai.ac.jp>
2020-10-20 02:06:12 +0000

    message

---
+++ ex6-5.js
@@ -0,0 +1,12 @@
+var who = 'world';
+function sayhello() {
+    alert('Hello, ' + who + '!');
+}
+function taro() {
+    who = '太郎';
+    alert('太郎が来た' );
+}
+function hanako() {
+    who = '花子';
+    alert('花子が来た' );
+}
